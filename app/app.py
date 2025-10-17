6ef824f9cdce80695205bbfcbb1429a3424a5ac6
jpe793 <jpe793@users.noreply.github.com>
2025-10-17 21:19:59 +0200

    feat: nuevo mensaje

--- app/app.py
+++ app/app.py
@@ -2,6 +2,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return jsonify({"status": "ok", "message": "Hello from CI/CD on Kubernetes!"})
+    return jsonify({"status": "ok", "message": "Hello from Jenkins CI/CD!"})
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
